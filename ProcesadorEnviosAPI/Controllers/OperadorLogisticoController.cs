81c6e5a6df947e9e1e2b95a256be6ba006f9148d
JoaquinBassano <joaquinbas98@gmail.com>
2021-10-28 15:41:30 -0300

    Get listo aunque no ande la conexion

--- ProcesadorEnviosAPI/Controllers/OperadorLogisticoController.cs
+++ ProcesadorEnviosAPI/Controllers/OperadorLogisticoController.cs
@@ -1,5 +1,12 @@
-using ProcesadorEnviosAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using ProcesadorEnviosAPI.Models;
 
 namespace ProcesadorEnviosAPI.Controllers
 {
@@ -7,10 +14,17 @@ namespace ProcesadorEnviosAPI.Controllers
     [ApiController]
     public class OperadoresLogisticosController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetAll()
+        private readonly MariaDbContext _context;
+
+        public OperadoresLogisticosController(MariaDbContext context)
         {
-            return Ok();
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OperadorLogistico>>> GetAll()
+        {
+            return await _context.operadoresLogisticos.ToListAsync();
         }
 
         [Route("{operadorLogisticoId}")]
