59528655f664d8dcdc62b2b4a0a3784a31ea0669
Maximiliano Burgos <maxibliz6@gmail.com>
2021-11-10 21:19:15 -0300

    Faltan decorados y poner los clientes de hacer falta

--- ProcesadorEnviosAPI/Startup.cs
+++ ProcesadorEnviosAPI/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,20 +32,45 @@ namespace ProcesadorEnviosAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            string domain = Configuration["Auth0:Domain"];
+
             services.AddDbContext<ApiContext>(
                 options => 
                     options.UseMySql(Configuration.GetConnectionString("BDConnectionString"),
                     ServerVersion.AutoDetect(Configuration.GetConnectionString("BDConnectionString"))
                 )
             );
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = domain;
+                options.Audience = Configuration["Auth0:Audience"] ;
+            });
             
+             services.AddAuthorization(options =>
+            {
+                options.AddPolicy("write:envios", policy => policy.Requirements.Add(new HasScopeRequirement("write:envios", domain)));
+                options.AddPolicy("read:envios", policy => policy.Requirements.Add(new HasScopeRequirement("read:envios", domain)));
+                options.AddPolicy("write:novedades", policy => policy.Requirements.Add(new HasScopeRequirement("write:novedades", domain)));
+                options.AddPolicy("write:operadores", policy => policy.Requirements.Add(new HasScopeRequirement("write:operadores", domain)));
+                options.AddPolicy("read:operadores", policy => policy.Requirements.Add(new HasScopeRequirement("read:operadores", domain)));
+                options.AddPolicy("delete:operadores", policy => policy.Requirements.Add(new HasScopeRequirement("delete:operadores", domain)));
+
+            });
+
             /*
             services.AddDbContext<ApiContext>(opt => // Agregar
                                                opt.UseInMemoryDatabase("TodoList"));
                                                */
-
             services.AddControllers();
+
+            // Register the scope authorization handler
+            services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProcesadorEnviosAPI", Version = "v1" });
@@ -63,7 +90,7 @@ namespace ProcesadorEnviosAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
